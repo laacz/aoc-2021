8b5b29b9f6c98bc813218053576a9a8c9d32cec3
Kaspars Foigts <laacz@laacz.lv>
2021-12-16 23:18:21 +0200

    Day 16; small refactor Get rid of globals, refactor case to match, etc.

--- 16.php
+++ 16.php
@@ -16,80 +16,57 @@ if (strlen(str_replace(['0', '1'], '', $msg))) {
     $msg = implode('', array_map(fn($char) => substr('000' . decbin(hexdec($char)), -4), str_split($msg)));
 }
 
-$result1 = 0;
-$pos = 0;
-
-function decode(string $bits, $packet_limit = 0, $pos_limit = 0, $level = 0)
+function decode(string $bits, $packet_limit = 0, $pos_limit = 0): array
 {
-    global $result1, $pos;
-    $prefix = str_repeat('  ', $level);
+    $versions = [];
     $packets_decoded = 0;
+    $pos = 0;
     $ret = [];
     while ($pos + 6 < strlen($bits)) {
-        $version = bindec(substr($bits, $pos, 3));
-        $result1 += $version;
-        $pos += 3;
-        $type_id = bindec(substr($bits, $pos, 3));
-        $pos += 3;
+        $versions[] = bindec(substr($bits, $pos, 3));
+        $type_id = bindec(substr($bits, $pos + 3, 3));
+        $pos += 6;
         switch ($type_id) {
             case 4:
                 $value = '';
                 while (true) {
                     $part = substr($bits, $pos, 5);
                     $pos += 5;
-
                     $value .= substr($part, 1);
-
                     if ($part[0] === '0') {
                         break;
                     }
                 }
-                echo "{$prefix}v$version Value $value => " . bindec($value) . "\n";
                 $ret[] = bindec($value);
                 break;
             default:
                 $length_type_id = $bits[$pos];
                 $pos += 1;
-                $operator = ['sum', 'mul', 'min', 'max', null, 'gt', 'lt', 'eq'][$type_id];
-                switch ($length_type_id) {
-                    case '0':
-                        $length = bindec(substr($bits, $pos, 15));
-                        $pos += 15;
-                        echo "{$prefix}v$version operator $operator with sub-packets length of $length bit(s)\n";
-                        $values = decode($bits, 0, $pos + $length, $level + 1);
-                        break;
-                    case '1':
-                        $number = bindec(substr($bits, $pos, 11));
-                        $pos += 11;
-                        echo "{$prefix}v$version operator $operator with $number packet(s)\n";
-                        $values = decode($bits, $number, 0, $level + 1);
-                        break;
+
+                $length = $number = 0;
+                if ($length_type_id == '0') {
+                    $length = bindec(substr($bits, $pos, 15));
+                    $pos += 15;
+                } else {
+                    $number = bindec(substr($bits, $pos, 11));
+                    $pos += 11;
                 }
-                echo "$prefix  $operator(" . implode(', ', $values) . ")\n";
-                switch ($operator) {
-                    case 'sum':
-                        $ret[] = array_sum($values);
-                        break;
-                    case 'mul':
-                        $ret[] = array_reduce($values, fn($a, $b) => $a * $b, 1);
-                        break;
-                    case 'min':
-                        $ret[] = min($values);
-                        break;
-                    case 'max':
-                        $ret[] = max($values);
-                        break;
-                    case 'gt':
-                        $ret[] = (int)($values[0] > $values[1]);
-                        break;
-                    case 'lt':
-                        $ret[] = (int)($values[0] < $values[1]);
-                        break;
-                    case 'eq':
-                        $ret[] = (int)($values[0] == $values[1]);
-                        break;
-                    default:
-                        die("Unknown operator with type_id $type_id");
+                [$values, $next_pos, $next_versions] = decode(substr($bits, $pos), $number, $length);
+                $pos += $next_pos;
+                $versions = array_merge($versions, $next_versions);
+
+                $operator = ['sum', 'mul', 'min', 'max', null, 'gt', 'lt', 'eq'][$type_id];
+
+                if (count($values)) {
+                    $ret[] = match ($operator) {
+                        'sum' => array_sum($values),
+                        'mul' => array_reduce($values, fn($a, $b) => $a * $b, 1),
+                        'min' => min($values),
+                        'max' => max($values),
+                        'gt' => (int)($values[0] > $values[1]),
+                        'lt' => (int)($values[0] < $values[1]),
+                        'eq' => (int)($values[0] == $values[1]),
+                    };
                 }
         }
         $packets_decoded++;
@@ -100,10 +77,13 @@ function decode(string $bits, $packet_limit = 0, $pos_limit = 0, $level = 0)
             break;
         }
     }
-    return $ret;
+    return [$ret, $pos, $versions];
 }
 
-$result2 = decode($msg);
+$result = decode($msg);
+
+$result1 = array_sum($result[2]);
+$result2 = $result[0][0];
 
 echo "Result1: $result1\n";
-echo "Result2: " . array_shift($result2) . "\n";
+echo "Result2: " . $result2 . "\n";
